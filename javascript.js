4b5fa0ddb21b220ad648c885474c10e876f29813
Srushtipatel14 <srushtip579@gmail.com>
2025-08-20 23:02:06 +0530

    done

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("list_2").addEventListener("click", function () {
         console.log("grandparent")
-    },true)
+    },false)
 
     document.getElementById("list_1").addEventListener("click", function () {
         console.log("parent")
@@ -11,3 +11,5 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("child")
     },false)
 })
+
+//by default event bubbling happen from bottom to top
