fb2f383137767a2f3d955b99f4b317c7c483c407
Srushtipatel14 <srushtip579@gmail.com>
2025-08-20 22:39:08 +0530

    a nested object into a single-level object

--- javascript.js
+++ javascript.js
@@ -1,14 +1,25 @@
-function val(a){
-    return function(b){
-        if(b===undefined){
-            return console.log(a)
-        }
-        else{
-            return val(a*b)
+const detail={
+    fname:"srushti",
+    lname:"patel",
+    address:{
+        village:"kansa",
+        taluka:"visnagar"
+    }
+
+};
+
+function convertObject(detail,prefix="detail"){
+    let result={}
+    for(let key in detail){
+        let newkey=`${prefix}_${key}`;
+        if(typeof  detail[key]==="object" && !Array.isArray(detail[key]) && detail[key]!==null ){
+            Object.assign(result,convertObject(detail[key],newkey))
+        }else{
+            
+        result[newkey]=detail[key]
         }
     }
+    return result
 }
 
-val(1)(2)(3)(4)()
-
-val(2)(6)(3)(4)(1)()
+console.log(convertObject(detail))
