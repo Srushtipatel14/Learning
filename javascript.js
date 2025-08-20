432f53337009a7da52d422ac07194b069635adf9
Srushtipatel14 <srushtip579@gmail.com>
2025-08-20 21:26:32 +0530

    example of polyfill for bind method

--- javascript.js
+++ javascript.js
@@ -9,15 +9,29 @@ function valprint(v1,v2,v3){
     console.log(this.fname,this.lname,v1,v2,v3)
 }
 
-//call method invokes immediately, takes arguments as comma seperated values
-valprint.call(details,"v1","v2","v3")
+// //call method invokes immediately, takes arguments as comma seperated values
+// valprint.call(details,"v1","v2","v3")
 
 
-//apply method invokes immediately, takes arguments as array or array like objects
+// //apply method invokes immediately, takes arguments as array or array like objects
 
-valprint.apply(details,["v1","v2","v3"])
+// valprint.apply(details,["v1","v2","v3"])
 
-// in bind menthod return a function and call it whenever need , takes arguments like call method
+// // in bind menthod return a function and call it whenever need , takes arguments like call method
 
-const bindmethod=valprint.bind(details,"v1","v2","v3")
-bindmethod()
+// const bindmethod=valprint.bind(details,"v1","v2","v3")
+// bindmethod();
+
+
+//example of poly fill for bind method
+
+Function.prototype.mybind=function(...args){
+    obj=this;
+    param=args.slice(1)
+    return function(...arg){
+        obj.apply(args[0],[...param,...arg])
+    }
+}
+
+const bindmethod=valprint.mybind(details,"v1","v2")
+bindmethod("v3");
