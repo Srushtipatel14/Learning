d70417574be8807ccbe22cd6ee4310078889f9e0
Srushtipatel14 <srushtip579@gmail.com>
2025-09-03 22:29:23 +0530

    removing all zeros from the array without using any iterator method

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
-const heading = React.createElement(
-    "div", 
-    {id:"parent"},
-    React.createElement("div",{id:"child1"},"child1"),
-    React.createElement("div",{id:"child2"},"child2"))
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+//removing all zeros from the array without using any iterator method
+
+const a=[1,2,0,3,0,4,0,5,0,6,7,8,0,0,9];
+let result=[];
+
+function val(arr){
+    const temp=arr;
+    if(arr.length===0){
+        return result;
+    }
+    else{
+        const val1=temp.slice(0,1)
+        const v=temp.slice(1)
+        if(val1[0]!==0){
+            result.push(val1[0]);
+        }
+        return val(v)
+    }
+}
+
+console.log(val(a));
