ab8ee3b0bca30f1b22f83811a5e3cfea98a40f9e
Srushtipatel14 <srushtip579@gmail.com>
2025-09-04 20:37:08 +0530

    find reason for this program why happen

--- javascript.js
+++ javascript.js
@@ -1,21 +1,27 @@
-//removing all zeros from the array without using any iterator method
+// //removing all zeros from the array without using any iterator method
 
-const a=[1,2,0,3,0,4,0,5,0,6,7,8,0,0,9];
-let result=[];
+// const a=[1,2,0,3,0,4,0,5,0,6,7,8,0,0,9];
+// let result=[];
 
-function val(arr){
-    const temp=arr;
-    if(arr.length===0){
-        return result;
-    }
-    else{
-        const val1=temp.slice(0,1)
-        const v=temp.slice(1)
-        if(val1[0]!==0){
-            result.push(val1[0]);
-        }
-        return val(v)
-    }
-}
+// function val(arr){
+//     const temp=arr;
+//     if(arr.length===0){
+//         return result;
+//     }
+//     else{
+//         const val1=temp.slice(0,1)
+//         const v=temp.slice(1)
+//         if(val1[0]!==0){
+//             result.push(val1[0]);
+//         }
+//         return val(v)
+//     }
+// }
 
-console.log(val(a));
+// console.log(val(a));
+
+
+const a=[1,2,3]
+const b=a;
+b.push(4)
+console.log(a)
