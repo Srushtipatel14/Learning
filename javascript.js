a8b0da9ed2cc2df4b0a4ac212f6d8a18087bd7a7
Srushtipatel14 <srushtip579@gmail.com>
2025-09-13 22:21:11 +0530

    promise types all,allSettled,any,race

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 // //removing all zeros from the array without using any iterator method
 
+const { resolve } = require("path");
+
 // const a=[1,2,0,3,0,4,0,5,0,6,7,8,0,0,9];
 // let result=[];
 
@@ -21,7 +23,31 @@
 // console.log(val(a));
 
 
-const a=[1,2,3]
-const b=a;
-b.push(4)
-console.log(a)
+// const a=[1,2,3]
+// const b=a;
+// b.push(4)
+// console.log(a)
+
+const p1=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("p1 resolve")
+    },1000)
+})
+
+const p2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("p2 resolve")
+    },3000)
+})
+
+const p3=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("p3 resolve")
+    },5000)
+})
+
+const p=[p1,p2,p3];
+
+Promise.any(p)
+.then((msg)=>console.log(msg))
+.catch((err)=>console.log(err))
