a835e6a74de5bfa2118613c90cdc3d28251c04e0
Srushtipatel14 <srushtip579@gmail.com>
2025-10-05 21:36:25 +0530

    code for event delegation

--- javascript.js
+++ javascript.js
@@ -1,19 +1,7 @@
-const details={
-    fname:"srushti",
-    lname:"patel"
-}
-
-function detailsShow(v1,v2,v3){
-    return console.log(this.fname,this.lname,v1,v2,v3)
-}
-
-Function.prototype.mybind=function(...args){
-    params=args.splice(1)
-    obj=this;
-    return function(...arg){
-        obj.apply(args[0],[...params,...arg])
-    }
-}
-
-const val=detailsShow.mybind(details,"a","b")
-val("c");
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("container").addEventListener("click",function(e){
+        if(e.target.tagName==="LI"){
+            console.log(e.target.innerHTML)
+        }
+    })
+})
