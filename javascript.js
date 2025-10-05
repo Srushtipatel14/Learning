1c72df5bbca0dcb7103bf6630e2bcc34be491e0e
Srushtipatel14 <srushtip579@gmail.com>
2025-10-05 21:22:12 +0530

    code for polyfill for bind method

--- javascript.js
+++ javascript.js
@@ -7,5 +7,13 @@ function detailsShow(v1,v2,v3){
     return console.log(this.fname,this.lname,v1,v2,v3)
 }
 
-const val=detailsShow.bind(details,"a","b","c")
-val()
+Function.prototype.mybind=function(...args){
+    params=args.splice(1)
+    obj=this;
+    return function(...arg){
+        obj.apply(args[0],[...params,...arg])
+    }
+}
+
+const val=detailsShow.mybind(details,"a","b")
+val("c");
