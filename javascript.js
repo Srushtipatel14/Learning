46aa9fa416c84075cff3d8660007c0827eadfa9e
Srushtipatel14 <srushtip579@gmail.com>
2025-10-06 14:40:29 +0530

    code for throttling functionality

--- javascript.js
+++ javascript.js
@@ -1,18 +1,19 @@
-function throttling(callback,time){
-    let timer;
-    return function(...args){
-        clearTimeout(timer)
-        timer=setTimeout(()=>{
+function throttling(callback, time) {
+    let last = 0;
+    return function (...args) {
+        const now = Date.now()
+        if (now - last >= time) {
             callback(...args)
-        },time)
+            last = now;
+        }
     }
 }
 
-document.addEventListener("DOMContentLoaded",function(){
-    const functionCall=throttling((data)=>{
+document.addEventListener("DOMContentLoaded", function () {
+    const functionCall = throttling((data) => {
         console.log(data)
-    },200)
-    document.getElementById("container").addEventListener("input",function(e){
+    }, 1000)
+    document.getElementById("container").addEventListener("input", function (e) {
         functionCall(e.target.value)
     })
 })
