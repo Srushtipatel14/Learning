04f9ef7064a6987763354a10c4be4f4c108485bf
Srushtipatel14 <srushtip579@gmail.com>
2025-10-07 11:26:13 +0530

    add object

--- javascript.js
+++ javascript.js
@@ -1,12 +1,9 @@
-function recursion(a) {
-    return function (b) {
-        if (b === undefined) {
-            return console.log(a)
-        }
-        else {
-            return recursion(a * b)
-        }
+const details={
+    fname:"srushti",
+    lname:"patel",
+    address:{
+        village:"kansa",
+        taluka:"visnagar",
+        district:"mahesana"
     }
 }
-
-recursion(1)(2)(3)(5)(6)()
