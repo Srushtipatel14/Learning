2c7728b155980087583a06ffa1d02a12dc11dc5e
Srushtipatel14 <srushtip579@gmail.com>
2025-10-07 11:36:35 +0530

    convert object code

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
-const details={
-    fname:"srushti",
-    lname:"patel",
-    address:{
-        village:"kansa",
-        taluka:"visnagar",
-        district:"mahesana"
+const details = {
+    fname: "srushti",
+    lname: "patel",
+    address: {
+        village: "kansa",
+        taluka: "visnagar",
+        district: "mahesana"
     }
 }
+
+function convertObject(obj, prefix = "details") {
+    let result = {};
+    for (let key in obj) {
+        const newkey=`${prefix}_${key}`
+        if (typeof obj[key] === "object" && !Array.isArray(obj[key]) && obj[key] !== null) {
+            Object.assign(result,convertObject(obj[key],newkey))
+        }
+        else{
+            result[newkey]=obj[key]
+        }
+    }
+    return result;
+}
+
+console.log(convertObject(details))
