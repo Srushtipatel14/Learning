fb6ca2322894be522cb41ec1a1ea4ca8ce86e9b9
Srushtipatel14 <srushtip579@gmail.com>
2025-10-09 11:32:52 +0530

    delete  user done

--- server_action_demo/app/page.tsx
+++ server_action_demo/app/page.tsx
@@ -1,17 +1,22 @@
-import {actions} from "./api/all-in-one/route"
+import { actions } from "./api/all-in-one/route"
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <form action={actions.createUser} className=" d-flex gap-2" >
         <input type="text" name="name" className="border p-2" required />
-        <input type="text" name="email" className="border p-2" required  />
+        <input type="text" name="email" className="border p-2" required />
         <button type="submit" className="border p-2">Create</button>
       </form>
 
-        <form action={actions.updateUser} className=" d-flex gap-2" >
+      <form action={actions.updateUser} className=" d-flex gap-2" >
         <input type="text" name="name" className="border p-2" required />
-        <input type="text" name="id" className="border p-2" required  />
+        <input type="text" name="id" className="border p-2" required />
         <button type="submit" className="border p-2">Update</button>
+      </form>
+
+      <form action={actions.deleteUser} className=" d-flex gap-2" >
+        <input type="text" name="id" className="border p-2" required />
+        <button type="submit" className="border p-2">Delete</button>
       </form>
 
     </div>
