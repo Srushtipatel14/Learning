056a4c3b4481fbfcc7fe3d3c3ed7017794698dbd
Srushtipatel14 <srushtip579@gmail.com>
2025-10-10 10:41:13 +0530

    done

--- server_action_demo/app/page.tsx
+++ server_action_demo/app/page.tsx
@@ -1,21 +1,25 @@
-import {actions} from "./api/all-in-one/route"
+import { actions } from "./api/all-in-one/route"
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+
+      {/* create user */}
       <form action={actions.createUser}>
         <input type="text" name="name" className="border p-2" />
         <input type="text" name="email" className="border p-2" />
         <button type="submit" className="border p-2">Create</button>
       </form>
 
-       <form action={actions.updateUser}>
+      {/* update user */}
+      <form action={actions.updateUser}>
         <input type="text" name="name" className="border p-2" />
         <input type="text" name="id" className="border p-2" />
         <button type="submit" className="border p-2">Update</button>
       </form>
 
-        <form action={actions.deleteUser}>
+      {/* delete user */}
+      <form action={actions.deleteUser}>
         <input type="text" name="id" className="border p-2" />
         <button type="submit" className="border p-2">Delete</button>
       </form>
