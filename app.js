ab7c9e6266ee11ecb6d163795fae49544a6ff9dd
Srushtipatel14 <srushtip579@gmail.com>
2025-11-11 16:59:12 +0530

    example of debouncing

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded",function(){
-    document.getElementById("val").addEventListener("click",function(e){
-        if(e.target.tagName==="LI"){
-            console.log(e.target.innerHTML)
+function debouncing(fn, time) {
+    let timeval = 0;
+    return function (...args) {
+        clearTimeout(timeval)
+        timeval = setTimeout(() => {
+            fn(...args)
+        }, time)
+        if (timeval >= time) {
+            timeval = time;
         }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const fun = debouncing((val) => {
+        console.log(val)
+    }, 1000)
+
+    document.getElementById("val").addEventListener("input", function (e) {
+        fun(e.target.value)
     })
 })
