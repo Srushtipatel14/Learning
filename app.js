08c9be563b8515fac49d780c5d3939d383ca4205
Srushtipatel14 <srushtip579@gmail.com>
2025-11-11 16:41:01 +0530

    example of event delegation

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const details={
-    fname:"srushti",
-    lname:"patel"
-}
-
-function val(details){
-    return function(v1){
-        return function(v2){
-            return function(v3){
-                return console.log(details.fname,details.lname,v1,v2,v3)
-            }
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("val").addEventListener("click",function(e){
+        if(e.target.tagName==="LI"){
+            console.log(e.target.innerHTML)
         }
-    }
-}
-
-val(details)("a")("b")("c")
+    })
+})
