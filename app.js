a21e001a5ce83b3ac2c0abdaae74d2fd8d25a46b
Srushtipatel14 <srushtip579@gmail.com>
2025-11-12 15:42:26 +0530

    example of memoize function

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-function val(a,b,c){
-  return console.log(a+b+c)
-}
 
-const res=val.bind(this,5);
-res(10,15)
+
+const memoize = (val) => {
+  const res = {};
+  return function (...x) {
+    if (res[x]) {
+      console.log("value is get from cache", res[x]);
+      return
+    }
+
+    const result =val(...x);
+    res[x] = result;
+    console.log("value is get through calculation", result);
+    return
+  }
+}
+const val=memoize((a,b)=>a*b)
+
+val(4, 5)
+val(4, 5)
