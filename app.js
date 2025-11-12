07e32ec01a5392f05f4aa8c56339b880d1b6c823
Srushtipatel14 <srushtip579@gmail.com>
2025-11-12 11:57:08 +0530

    immediately invoke function

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 let a=0;
-function printval(){
+(function printval(){
     return console.log(++a)
-}
+})();
 
-
-printval()
-printval()
-
-//this is called impure function in which for same input get different output 
-//there is aim side effect for this
