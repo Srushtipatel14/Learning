92385eb80250eb91d183d005dc11b1f7dd0897fa
Srushtipatel14 <srushtip579@gmail.com>
2025-11-13 22:08:17 +0530

    done

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-function throttling(fn, delay) {
-    let lastCall = 0;
-    return function (...args) {
-        const now = Date.now();
-        if (now - lastCall >= delay) {
-            lastCall = now;
-            fn(...args);
-        }
-    };
+const b=function(){
+    console.log("world")
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    const fun = throttling((val) => {
-        console.log(val);
-    }, 1000);
-
-    document.getElementById("val").addEventListener("input", function (e) {
-        fun(e.target.value);
-    });
-});
+b()
