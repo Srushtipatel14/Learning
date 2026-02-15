3ab02ba8b9b1fb9145ab30356bad61c0af8aaefa
Srushtipatel14 <srushtip579@gmail.com>
2026-02-15 22:32:20 +0530

    commit

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded",function(){
-    document.getElementById("container").addEventListener("click",function(e){
-        if(e.target.tagName==="LI"){
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("container").addEventListener("click", function (e) {
+        if (e.target.tagName === "LI") {
             console.log(e.target.innerHTML)
         }
     })
