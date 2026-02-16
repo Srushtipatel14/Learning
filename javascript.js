8d11a03c6f0670e9412ca5be9aca0b053c8b5b10
Srushtipatel14 <srushtip579@gmail.com>
2026-02-16 20:30:57 +0530

    example of event propogation

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("container").addEventListener("click", function (e) {
+    document.getElementById("val").addEventListener("click", function (e) {
         if (e.target.tagName === "LI") {
             console.log(e.target.innerHTML)
         }
