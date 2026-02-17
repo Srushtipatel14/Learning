96e19f9e68ad63f0ce648a7f5a91a456628cf669
Srushtipatel14 <srushtip579@gmail.com>
2026-02-17 14:15:54 +0530

    old way of infinite scrolling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,70 @@
 import { useEffect, useRef, useState } from "react";
 
-function App() {
-  const [val, setVal] = useState("");
-  const inputRef=useRef(0)
+//https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}
 
-  function fetchData(){
-    console.log(val)
+
+
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false)
+  const [hasMore, setHasMore] = useState(true)
+
+  useEffect(() => {
+    if (hasMore) {
+      fetchData();
+    }
+  }, [page]);
+
+  async function fetchData() {
+    try {
+      setLoading(true)
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
+      const jsondata = await res.json();
+      if (jsondata.length === 0) {
+        setHasMore(false);
+        return;
+      }
+      setItems((prev) => [...prev, ...jsondata]);
+    } catch (error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+    }
   }
 
-  useEffect(()=>{
-    if(val){
-      const now=new Date();
-      if(now-inputRef.current>=500){
-        fetchData();
-        inputRef.current=now;
-      }
+  const handleInfiniteScroll = () => {
+    const totalHeight = document.documentElement.scrollHeight;
+    const viewHeight = window.innerHeight;
+    const scrollHeight = document.documentElement.scrollTop;
+    if (totalHeight <= viewHeight + scrollHeight + 1) {
+      setPage((prev) => prev + 1)
     }
-  },[val])
+  }
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleInfiniteScroll);
+    return () => window.removeEventListener("scroll", handleInfiniteScroll)
+  }, [hasMore])
 
   return (
     <div className="App">
-      <input onChange={(e) => { setVal(e.target.value) }} value={val || ''} name="val" />
+      <div>
+        {items.map((item) => (
+          <div key={item.id} style={{ padding: "20px", border: "1px solid black" }}>{item.title}</div>
+        ))}
+      </div>
+      <div>
+        {loading && (
+          <p style={{ height: "20px", background: "grey" }} >loading...</p>
+        )}
+      </div>
+
+      {!hasMore && (
+        <p style={{ height: "20px", background: "grey" }} >no more data</p>
+      )}
     </div>
   );
 }
