6f361bd6843f733e1c2ee8b0614c32b178046705
Srushtipatel14 <srushtip579@gmail.com>
2026-02-17 13:39:11 +0530

    example of debouncing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,90 +1,24 @@
-import { useEffect, useRef, useState } from "react";
-
+import { useEffect, useState } from "react";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false)
-  const [hasMore, setHasMore] = useState(true)
-  const inputref=useRef(null)
+  const [val, setVal] = useState("")
 
-  useEffect(() => {
-    if (hasMore) {
-      fetchData()
-    }
-  }, [page]);
-
-  const fetchData = async () => {
-
-    setLoading(true)
-    try {
-      const resdata = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
-      const data = await resdata.json();
-      if (data.length === 0) {
-        setHasMore(false)
-        return;
-      }
-      setItems((prev) => [...prev, ...data])
-
-    } catch (error) {
-      console.log(error)
-    }
-    finally {
-      setLoading(false)
-    }
+  function fetchData(){
+    console.log(val)
   }
 
-  // const handleInfiniteScroll = () => {
-  //   if (!hasMore || loading) return;
-  //   const totalHeight = document.documentElement.scrollHeight;
-  //   const viewHeight = window.innerHeight;
-  //   const scrollHeight = document.documentElement.scrollTop;
-  //   if (totalHeight <= viewHeight + scrollHeight + 1) {
-  //     setPage((prev) => prev + 1)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleInfiniteScroll)
-  //   return () => window.removeEventListener("scroll", handleInfiniteScroll)
-  // }, [loading, hasMore])
-  
   useEffect(()=>{
-    const observer=new IntersectionObserver((entry)=>{
-      if(entry[0].isIntersecting && !loading){
-        setPage((prev)=>prev+1);
-      }
-    },{threshold:1})
-    if(inputref.current){
-      observer.observe(inputref.current)
-    }
-
-    return ()=>{
-      if(inputref.current){
-        observer.unobserve(inputref.current)
-      }
-    }
-
-
-  },[loading,hasMore])
+    const timer=setTimeout(()=>{
+      if(!val || val==='' ) return;
+      fetchData()
+    },500)
+    return ()=>clearTimeout(timer);
+  },[val])
 
 
   return (
     <div className="App">
-      <div>
-        {items.map((item) => (
-          <div key={item.id} style={{ padding: "20px", border: "1px solid black" }}>{item.title}</div>
-        ))}
-      </div>
-       <div
-        ref={inputref}
-        style={{ height: "50px", textAlign: "center" }}
-      >
-        {loading && <p>Loading...</p>}
-      </div>
-
-
-      {!hasMore && <p style={{ textAlign: "center" }}>No more data</p>}
+      <input onChange={(e) => { setVal(e.target.value) }} value={val || ''} name="val" />
     </div>
   );
 }
