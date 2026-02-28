d0773dc8f8566fe187557fdc3b747df7e4b4f9c6
Srushtipatel14 <srushtip579@gmail.com>
2026-02-28 12:09:16 +0530

    example of infinite screolling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,70 @@ import { useEffect, useRef, useState } from "react";
 //https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}
 
 function App() {
-  const [inval,setInval]=useState("");
-  const lastTime=useRef(0)
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState([]);
+  const inputRef = useRef(null);
+  const [loading, setLoading] = useState(false)
+  const [hasMore, setHasMore] = useState(true);
 
-  const fetchData=()=>{
-    console.log(inval)
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const resdata = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
+      const data = await resdata.json();
+      if(data.length===0){
+        setHasMore(false);
+        return
+      }
+      setItems((prev) => [...prev, ...data])
+    } catch (error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+    }
   }
 
   useEffect(()=>{
-    if(inval==='') return;
-    const time=new Date();
-    if(time-lastTime.current>=500){
-      fetchData()
-      lastTime.current=time;
-    }
-  },[inval])
+    const observer=new IntersectionObserver((enty)=>{
+      if(enty[0].isIntersecting && !loading){
+        setPage((prev)=>prev+1)
+      }
+    },{threshold:1})
 
+    const obj=inputRef.current;
+    if(obj){
+      observer.observe(obj)
+    }
+    return ()=>{
+      if(obj){
+        observer.unobserve(obj);
+      }
+    }
+
+  },[hasMore,loading])
+
+  useEffect(() => {
+    if (hasMore) {
+      fetchData();
+    }
+  }, [page])
   return (
     <div className="App">
-      <input onChange={(e)=>setInval(e.target.value)}/>
+      <div>
+        {items.map((item, index) => (
+          <div style={{ padding: "20px", border: "1px solid black" }} key={index}>{index + 1}.{item.title}</div>
+        ))}
+      </div>
+      <div ref={inputRef}>
+        {loading && (
+          <div style={{ padding: "20px", backgroundColor: "grey" }} >Loading...</div>
+        )}
+      </div>
+
+      {!hasMore && (
+        <div>No more data present</div>
+      )}
     </div>
   );
 }
