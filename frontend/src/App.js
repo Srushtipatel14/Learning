b1448233a2b4caeac2a354e1c9381c5bba000267
Srushtipatel14 <srushtip579@gmail.com>
2026-02-28 22:33:04 +0530

    debouncing example done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,89 +2,24 @@ import { useEffect, useRef, useState } from "react";
 //https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}
 
 function App() {
-  const [page, setPage] = useState(1);
-  const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(false)
-  const [hasMore, setHasMore] = useState(true)
-  const inputref=useRef(null)
+  const [inputVal, setInputVal] = useState('');
 
-  useEffect(() => {
-    if (hasMore) {
-      fetchData()
-    }
-  }, [page]);
-
-  const fetchData = async () => {
-
-    setLoading(true)
-    try {
-      const resdata = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
-      const data = await resdata.json();
-      if (data.length === 0) {
-        setHasMore(false)
-        return;
-      }
-      setItems((prev) => [...prev, ...data])
-
-    } catch (error) {
-      console.log(error)
-    }
-    finally {
-      setLoading(false)
-    }
+  const fetchdata =() => {
+    console.log(inputVal)
   }
 
-  // const handleInfiniteScroll = () => {
-  //   if (!hasMore || loading) return;
-  //   const totalHeight = document.documentElement.scrollHeight;
-  //   const viewHeight = window.innerHeight;
-  //   const scrollHeight = document.documentElement.scrollTop;
-  //   if (totalHeight <= viewHeight + scrollHeight + 1) {
-  //     setPage((prev) => prev + 1)
-  //   }
-  // }
+  useEffect(() => {
+    const time = setTimeout(() => {
+      if (inputVal === '') return;
+      fetchdata();
+    }, 500);
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleInfiniteScroll)
-  //   return () => window.removeEventListener("scroll", handleInfiniteScroll)
-  // }, [loading, hasMore])
-  
-  useEffect(()=>{
-    const observer=new IntersectionObserver((entry)=>{
-      if(entry[0].isIntersecting && !loading){
-        setPage((prev)=>prev+1);
-      }
-    },{threshold:1})
-    if(inputref.current){
-      observer.observe(inputref.current)
-    }
-
-    return ()=>{
-      if(inputref.current){
-        observer.unobserve(inputref.current)
-      }
-    }
-
-
-  },[loading,hasMore])
-
+    return () => clearTimeout(time);
+  }, [inputVal])
 
   return (
     <div className="App">
-      <div>
-        {items.map((item) => (
-          <div key={item.id} style={{ padding: "20px", border: "1px solid black" }}>{item.title}</div>
-        ))}
-      </div>
-       <div
-        ref={inputref}
-        style={{ height: "50px", textAlign: "center" }}
-      >
-        {loading && <p>Loading...</p>}
-      </div>
-
-
-      {!hasMore && <p style={{ textAlign: "center" }}>No more data</p>}
+      <input onChange={(e) => setInputVal(e.target.value)} />
     </div>
   );
 }
