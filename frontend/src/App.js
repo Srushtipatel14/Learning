6f9e53441f7f608b54ea181d42dfe752ad5ab14c
Srushtipatel14 <srushtip579@gmail.com>
2026-02-28 11:25:02 +0530

    example of debouncing is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,75 +1,24 @@
 import { useEffect, useRef, useState } from "react";
 
-//https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}
-
 function App() {
-  const [items, setItems] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false)
-  const [hasMore, setHasMore] = useState(true);
-  const refVal = useRef()
+  const [inval,setInval]=useState("");
 
-  useEffect(() => {
-    if (hasMore) {
-      fetchData();
-    }
-  }, [page]);
-
-  async function fetchData() {
-    try {
-      setLoading(true)
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
-      const jsondata = await res.json();
-      if (jsondata.length === 0) {
-        setHasMore(false);
-        return;
-      }
-      setItems((prev) => [...prev, ...jsondata]);
-    } catch (error) {
-      console.log(error)
-    }
-    finally {
-      setLoading(false)
-    }
+  const fetchData=()=>{
+    console.log(inval)
   }
 
-  useEffect(() => {
-    if (!hasMore) return;
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting && !loading && hasMore) {
-        setPage((prev) => prev + 1)
-      }
-    }, { threshold: 0,root:null,rootMargin:"100px" });
-    const el=refVal.current
+  useEffect(()=>{
+    if(inval==='') return;
+    const timer=setTimeout(()=>{
+      fetchData()
+    },500);
 
-    if (el) {
-      observer.observe(el)
-    }
-    return () => {
-      if (el) {
-        observer.unobserve(el)
-      }
-    }
+    return ()=>clearTimeout(timer)
+  },[inval])
 
-  }, [hasMore, loading])
-
-  
   return (
     <div className="App">
-      <div>
-        {items.map((item) => (
-          <div key={item.id} style={{ padding: "20px", border: "1px solid black" }}>{item.title}</div>
-        ))}
-      </div>
-      <div ref={refVal}>
-        {loading && (
-          <p style={{ height: "20px", background: "grey" }} >loading...</p>
-        )}
-      </div>
-
-      {!hasMore && (
-        <p style={{ height: "20px", background: "grey" }} >no more data</p>
-      )}
+      <input onChange={(e)=>setInval(e.target.value)}/>
     </div>
   );
 }
