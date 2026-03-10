de63b8e12cbda3727d8fb6624029114e1a06ca12
Srushtipatel14 <srushtip579@gmail.com>
2026-03-10 21:37:36 +0530

    change

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
-const worker=new Worker("worker.js");
-
-document.addEventListener("DOMContentLoaded", function () {
-    const sum = document.getElementById("sum");
-    const color = document.getElementById("color");
-
-    sum.addEventListener("click",function(){
-        worker.postMessage("sum")
-    })
-
-    worker.onmessage=function(message){
-        alert(`sum is ${message.data}`)
-    }
-
-    color.addEventListener("click", function () {
-        if (document.body.style.backgroundColor !== 'red') {
-            document.body.style.backgroundColor = 'red'
-        }
-        else {
-            document.body.style.backgroundColor = 'white';
-        }
-    });
-})
+console.log(x)
+var x;
--- worker.js
+++ worker.js
@@ -1,9 +1,5 @@
 self.onmessage = function (message) {
-    if (message.data === "sum") {
-        let sum = 0;
-        for (let i = 0; i < 10000000000; i++) {
-            sum = sum + i;
-        }
-        self.postMessage(sum)
+    if (message.data === "start") {
+        self.postMessage("done")
     }
 }
