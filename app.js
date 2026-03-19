43a0dadcacad292a715df73ea16628a4a5eae66c
Srushtipatel14 <srushtip579@gmail.com>
2026-03-19 10:38:43 +0530

    example of worker pool

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const crypto = require('crypto');
+const crypto=require("crypto");
 
-console.time('total');
-
-for (let i = 0; i < 10; i++) {
-  console.time(`Task ${i}`);
-
-  crypto.pbkdf2('pass', 'salt', 5000000, 64, 'sha512', () => {
-    console.timeEnd(`Task ${i}`);
-  });
+for(let i=0;i<10;i++){
+    console.time(`task ${i}`)
+    crypto.pbkdf2("pass",'salt',5000000,64,"sha512",()=>{
+        console.timeEnd(`task ${i}`)
+    })
 }
