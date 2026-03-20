8a6e9156fea2f2189e696f26ddc3bf51bcf492f5
Srushtipatel14 <srushtip579@gmail.com>
2026-03-20 11:40:15 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 console.time('total');
 
 for (let i = 0; i < 10; i++) {
   console.time(`Task ${i}`);
-
+    
   crypto.pbkdf2('pass', 'salt', 5000000, 64, 'sha512', () => {
     console.timeEnd(`Task ${i}`);
   });
