1dd1063c0b086adab7f7021d73fe7cd82ffa6f6b
Srushtipatel14 <srushtip579@gmail.com>
2026-03-20 12:12:20 +0530

    example of event delegation

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const worker = new Worker("worker.js")
-
 document.addEventListener("DOMContentLoaded", function () {
-
-    worker.onmessage = function (msg) {
-        alert(msg.data)
-    }
-    document.getElementById("sum").addEventListener("click", function () {
-        worker.postMessage("sum");
-    })
-    document.getElementById("color").addEventListener("click", function () {
-        if (document.body.style.backgroundColor !== "red") {
-            document.body.style.backgroundColor = 'red'
-        }
-        else {
-            document.body.style.backgroundColor = 'white'
+    document.getElementById("list").addEventListener("click",function(e){
+        if(e.target.tagName==='LI'){
+            console.log(e.target.innerHTML)
         }
     })
 })
