ef941a8a4900b75e20f6292cb6036e0caa36e897
Srushtipatel14 <srushtip579@gmail.com>
2026-03-25 21:11:37 +0530

    example of traditional infinite scrolling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,69 @@ import { useEffect, useRef, useState } from "react";
 
 function App() {
 
-  const [inpval,setInpVal]=useState("");
-  const val=useRef(0)
+  const [page, setPage] = useState(1);
+  const [item, setItem] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
-  const fetchData=()=>{
-    console.log(inpval)
+  const handleScroll = () => {
+    if (loading && hasMore) return;
+    const totalHeight = document.documentElement.scrollHeight;
+    const viewHeight = window.innerHeight;
+    const scrollHeight = document.documentElement.scrollTop;
+    if (totalHeight - 10 <= viewHeight + scrollHeight) {
+      setPage((prev) => prev + 1)
+    }
   }
 
-  const handlechange=(e)=>{
-    const {value}=e.target;
-    setInpVal(value)
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+
+  }, [loading, hasMore]);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const resData = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
+      const data = await resData.json();
+
+      if (data.length === 0) {
+        setHasMore(false);
+        return
+      }
+      setItem((prev) => [...prev, ...data])
+    } catch (error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+    }
   }
 
-  useEffect(()=>{
-   const time=new Date();
-   if(time-val.current>=500){
-    fetchData();
-    val.current=time;
-   }
-  },[inpval]);
+  useEffect(() => {
+    if (hasMore) {
+      fetchData();
+    }
+  }, [page])
 
   return (
     <div className="App">
-      <input onChange={handlechange} value={inpval}/>
+      <div>
+        {item.map((val, index) => (
+          <div style={{ height: "50px", border: "1px solid black", padding: "20px" }}>{val.title}</div>
+        ))}
+      </div>
+
+      {loading && (
+        <div style={{ height: "30px", border: "1px solid black", padding: "10px", backgroundColor: "grey" }}>loading...</div>
+      )}
+
+      {!hasMore && (
+        <div style={{ height: "30px", border: "1px solid black", padding: "10px" }}>No more data available</div>
+      )}
     </div>
   );
 }
