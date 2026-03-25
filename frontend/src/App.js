ffd20bcaebf15be728c11f26b5c9ca74ffbf43ec
Srushtipatel14 <srushtip579@gmail.com>
2026-03-25 21:24:34 +0530

    example of modern way  infinite scrolling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,24 +6,7 @@ function App() {
   const [item, setItem] = useState([]);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
-
-  const handleScroll = () => {
-    if (loading && hasMore) return;
-    const totalHeight = document.documentElement.scrollHeight;
-    const viewHeight = window.innerHeight;
-    const scrollHeight = document.documentElement.scrollTop;
-    if (totalHeight - 10 <= viewHeight + scrollHeight) {
-      setPage((prev) => prev + 1)
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
-
-  }, [loading, hasMore]);
+  const inputRef = useRef();
 
   const fetchData = async () => {
     try {
@@ -45,8 +28,24 @@ function App() {
   }
 
   useEffect(() => {
+    const observer = new IntersectionObserver((entry) => {
+      if (entry[0].isIntersecting && hasMore) {
+        setPage((prev) => prev + 1)
+      }
+    }, { threshold: 1 });
+
+    const val = inputRef.current;
+    if (val) {
+      observer.observe(val)
+    }
+    return () => {
+      observer.unobserve(val)
+    }
+  }, [hasMore, loading])
+
+  useEffect(() => {
     if (hasMore) {
-      fetchData();
+      fetchData()
     }
   }, [page])
 
@@ -58,10 +57,12 @@ function App() {
         ))}
       </div>
 
-      {loading && (
-        <div style={{ height: "30px", border: "1px solid black", padding: "10px", backgroundColor: "grey" }}>loading...</div>
-      )}
+      <div ref={inputRef}>
+        {loading && (
+          <div style={{ height: "30px", border: "1px solid black", padding: "10px", backgroundColor: "grey" }}>loading...</div>
+        )}
 
+      </div>
       {!hasMore && (
         <div style={{ height: "30px", border: "1px solid black", padding: "10px" }}>No more data available</div>
       )}
