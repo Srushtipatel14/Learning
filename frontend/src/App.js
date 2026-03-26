3bba28618b42b64740089fda32ccf6b5d983ee22
Srushtipatel14 <srushtip579@gmail.com>
2026-03-26 10:13:30 +0530

    example of debouncing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,71 +1,29 @@
 import { useEffect, useRef, useState } from "react";
-
+//https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}
 function App() {
 
-  const [page, setPage] = useState(1);
-  const [item, setItem] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
-  const inputRef = useRef();
+  const [inpval, setInpval] = useState("")
 
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const resData = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
-      const data = await resData.json();
-
-      if (data.length === 0) {
-        setHasMore(false);
-        return
-      }
-      setItem((prev) => [...prev, ...data])
-    } catch (error) {
-      console.log(error)
-    }
-    finally {
-      setLoading(false)
-    }
+  const fetchData = () => {
+    console.log(inpval)
   }
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entry) => {
-      if (entry[0].isIntersecting && hasMore) {
-        setPage((prev) => prev + 1)
-      }
-    }, { threshold: 1 });
-
-    const val = inputRef.current;
-    if (val) {
-      observer.observe(val)
-    }
-    return () => {
-      observer.unobserve(val)
-    }
-  }, [hasMore, loading])
-
-  useEffect(() => {
-    if (hasMore) {
+    const value = setTimeout(() => {
       fetchData()
+    }, 500)
+    return () => {
+      clearTimeout(value)
     }
-  }, [page])
+  }, [inpval])
 
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInpval(value)
+  }
   return (
     <div className="App">
-      <div>
-        {item.map((val, index) => (
-          <div style={{ height: "50px", border: "1px solid black", padding: "20px" }}>{val.title}</div>
-        ))}
-      </div>
-
-      <div ref={inputRef}>
-        {loading && (
-          <div style={{ height: "30px", border: "1px solid black", padding: "10px", backgroundColor: "grey" }}>loading...</div>
-        )}
-
-      </div>
-      {!hasMore && (
-        <div style={{ height: "30px", border: "1px solid black", padding: "10px" }}>No more data available</div>
-      )}
+      <input onChange={handleChange} value={inpval} />
     </div>
   );
 }
