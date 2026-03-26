ac315ee9de80b8ac054e5709f637c3b13a90ca09
Srushtipatel14 <srushtip579@gmail.com>
2026-03-26 10:44:00 +0530

    example of traditional infinite scrolling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,69 @@ import { useEffect, useRef, useState } from "react";
 //https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}
 function App() {
 
-  const [inpval, setInpval] = useState("")
-  const inpref=useRef(0)
+  const [loading, setLoading] = useState(false);
+  const [hasmore, setHasMore] = useState(true)
+  const [page, setPage] = useState(1)
+  const [item, setItem] = useState([])
+  const inputRef = useRef();
 
-  const fetchData = () => {
-    console.log(inpval)
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const getData = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
+      const data = await getData.json()
+
+      if (data.length === 0) {
+        setHasMore(false)
+        return;
+      }
+      setItem((prev) => [...prev, ...data])
+    } catch (error) {
+      console.log(error)
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  const handleScroll = () => {
+    const totalHeight = document.documentElement.scrollHeight;
+    const viewHeight = window.innerHeight;
+    const scrollHeight = document.documentElement.scrollTop;
+
+    if (totalHeight - 10 <= viewHeight + scrollHeight) {
+      if (loading && hasmore) return;
+      setPage((prev) => prev + 1)
+    }
   }
 
   useEffect(() => {
-    if(inpval==='') return;
-    const now=new Date()
-    if(now-inpref.current>=500){
-      fetchData();
-      inpref.current=now;
+    if (hasmore) {
+      fetchData()
     }
-  }, [inpval])
+  }, [page])
 
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setInpval(value)
-  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [loading, hasmore])
+
   return (
     <div className="App">
-      <input onChange={handleChange} value={inpval} />
+      <div>
+        {item.map((val, index) => (
+          <div key={index} style={{ padding: "30px", border: "1px solid black" }}>{val.title}</div>
+        ))}
+      </div>
+
+      {loading && (
+        <div style={{ padding: "30px", border: "1px solid black", backgroundColor: "green" }}>loading...</div>
+      )}
+      {!hasmore && (
+        <div style={{ padding: "30px", border: "1px solid black" }}>No more data available</div>
+      )}
     </div>
   );
 }
