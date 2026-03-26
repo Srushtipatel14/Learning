2701c049f8432474a035c6f073bf6764d31f172f
Srushtipatel14 <srushtip579@gmail.com>
2026-03-26 15:49:54 +0530

    chat loop with history

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,33 @@ require("dotenv").config()
 const { GoogleGenAI } = require("@google/genai");
 const readline = require("readline-sync")
 
-
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-
+const history = [];
 
 async function main() {
     const uservalue = readline.question("User Request : ")
+    history.push({
+        role: "user",
+        parts: [{ text: uservalue }]
+    })
 
-    const response = await ai.models.generateContent({
+    const chat = await ai.chats.create({
         model: "gemini-2.5-flash",
-        contents: uservalue,
+        history: history,
     });
-    console.log("AI Response : ", response.text);
-    main();
+
+    const response=await chat.sendMessage({
+        message:uservalue
+    })
+
+    console.log("AI Response : ",response.text);
+
+    history.push({
+        role: "model",
+        parts: [{ text: response.text }]
+    })
+
+    await main();
 }
+
 main();
