a1b250458c4dd34174d6bdb666a7525aa294d976
Srushtipatel14 <srushtip579@gmail.com>
2026-03-28 16:31:20 +0530

    example of child not render unnessasary with  prop and useMemo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
-export const Child = React.memo(function () {
-  console.log("child render")
+export const Child = React.memo(function (data) {
+  console.log("child render",data)
   return (<h1>Child</h1>
   )
 })
@@ -10,13 +10,18 @@ const App = () => {
 
   const [count, setCount] = useState(0)
   console.log("parent render")
+
+  const useData=useMemo(()=>{
+    return {name:"sruti"}
+  },[])
+  
   const handleClick = () => {
     setCount((prev) => prev + 1)
   }
   return (
     <div>
       <button onClick={handleClick}>count : {count}</button>
-      <Child />
+      <Child data={useData} />
     </div>
   )
 }
