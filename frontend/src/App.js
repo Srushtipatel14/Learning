0a6a95e7b5048acb1949509dd2f12a4472b02ebf
Srushtipatel14 <srushtip579@gmail.com>
2026-03-28 16:24:59 +0530

    example of child not render unnessasary with out props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,75 +1,24 @@
-import { useEffect, useRef, useState } from "react";
-//https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}
-function App() {
+import React, { useState } from "react";
 
-  const [loading, setLoading] = useState(false);
-  const [hasmore, setHasMore] = useState(true)
-  const [page, setPage] = useState(1)
-  const [item, setItem] = useState([])
-  const inputRef = useRef('');
+export const Child = React.memo(function () {
+  console.log("child render")
+  return (<h1>Child</h1>
+  )
+})
 
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const getData = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
-      const data = await getData.json()
+const App = () => {
 
-      if (data.length === 0) {
-        setHasMore(false)
-        return;
-      }
-      setItem((prev) => [...prev, ...data])
-    } catch (error) {
-      console.log(error)
-    }
-    finally {
-      setLoading(false)
-    }
+  const [count, setCount] = useState(0)
+  console.log("parent render")
+  const handleClick = () => {
+    setCount((prev) => prev + 1)
   }
-
-  useEffect(() => {
-    if(loading && !hasmore) return;
-    const observer = new IntersectionObserver((entry) => {
-      if (entry[0].isIntersecting && hasmore) {
-        setPage((prev) => prev + 1)
-      }
-    }, { threshold: 0.5 })
-
-    const val = inputRef.current;
-    if (val) {
-      observer.observe(val)
-    }
-
-    return () => {
-      observer.unobserve(val)
-    }
-  }, [hasmore, loading])
-
-  useEffect(() => {
-    if (hasmore) {
-      fetchData()
-    }
-  }, [page])
-
-
-
   return (
-    <div className="App">
-      <div>
-        {item.map((val, index) => (
-          <div key={index} style={{ padding: "30px", border: "1px solid black" }}>{val.title}</div>
-        ))}
-      </div>
-      <div ref={inputRef}>
-        {loading && (
-          <div style={{ padding: "30px", border: "1px solid black", backgroundColor: "green" }}>loading...</div>
-        )}
-      </div>
-      {!hasmore && (
-        <div style={{ padding: "30px", border: "1px solid black" }}>No more data available</div>
-      )}
+    <div>
+      <button onClick={handleClick}>count : {count}</button>
+      <Child />
     </div>
-  );
+  )
 }
 
 export default App;
