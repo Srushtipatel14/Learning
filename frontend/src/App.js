c5a6e1f9cdd88b4bdd8e74b57d157ff8875a50c2
Srushtipatel14 <srushtip579@gmail.com>
2026-03-31 15:23:12 +0530

    UI for redux implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import {useSelector,useDispatch} from "react-redux";
-import {increment,decrement} from "./features/counter/counterSlice"
-
 function App() {
-  const count=useSelector((state)=>state.counter.value);
-  const dispatch=useDispatch();
   return (
     <div className="App">
-    <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", padding: "30px", gap: "20px" }}>
+          <button style={{ borderStyle: "none", padding: "10px", borderRadius: "10px", backgroundColor: "lightGray" }}>Increment</button>
+          <button style={{ borderStyle: "none", padding: "10px", borderRadius: "10px", backgroundColor: "lightGray" }}>Decrement</button>
+        </div>
+        <div>
+          <h1>Count : 1</h1>
+        </div>
       </div>
     </div>
   )
