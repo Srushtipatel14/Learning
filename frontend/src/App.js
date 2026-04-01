0109dca1760b4efa57f1e5c158a0094112b51787
Srushtipatel14 <srushtip579@gmail.com>
2026-04-01 13:21:19 +0530

    example of render optimization using useMemo hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,74 +1,33 @@
-import { use, useEffect, useRef, useState } from "react";
+import React, { useMemo, useState } from "react";
+
+export const Child = React.memo((data) => {
+  console.log("child render",data)
+  return (
+    <h1>Child</h1>
+  )
+})
 
 function App() {
+  const [count, setCount] = useState(0)
 
-  const [items, setItem] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
-  const refval = useRef()
-
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const resData = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`);
-
-      const data = await resData.json();
-      if (data.length === 0) {
-        setHasMore(false)
-        return;
-      }
-      setItem((prev) => [...prev, ...data])
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setLoading(false)
-    }
+  const handleCount = () => {
+    setCount((prev) => prev + 1)
   }
+  console.log("parent render")
 
-  useEffect(() => {
-    if (hasMore) {
-      fetchData()
+  const userData = useMemo(() => {
+    return {
+      name: "Srushti"
     }
-  }, [page]);
-
-  useEffect(() => {
-    if (loading && !hasMore) return;
-
-    const observer = new IntersectionObserver((entry) => {
-      if (entry[0].isIntersecting && hasMore) {
-        setPage((prev) => prev + 1)
-      }
-    }, { threshold: 1 })
-
-    const val = refval.current;
-    if (val) {
-      observer.observe(val)
-    }
-    return () => {
-      observer.unobserve(val)
-    }
-  }, [loading, hasMore])
-
+  },[]);
 
   return (
     <div className="App">
-      <div>
-        {items.map((item, index) => (
-          <div key={index} style={{ padding: "30px", border: "1px solid black" }}>{item.title}</div>
-        ))}
-      </div>
+      <button onClick={handleCount}>Count : {count}</button>
 
-      <idv ref={refval}>
-        {loading && (
-          <div style={{ padding: "30px", border: "1px solid black", backgroundColor: "grey" }}>Loading...</div>
-        )}
-      </idv>
-
-      {!hasMore && (
-        <div style={{ padding: "30px", border: "1px solid black" }}>No more data available</div>
-      )}
+      <Child data={userData} />
     </div>
+
   )
 }
 
