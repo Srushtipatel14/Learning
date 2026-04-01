6f6dd36757762c9942a3f8b1823e5dc454e745d5
Srushtipatel14 <srushtip579@gmail.com>
2026-04-01 12:10:53 +0530

    example of throttling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
 
   const [inputVal, setInputVal] = useState("");
+  const inpref=useRef(0)
 
   const handleChnage = (e) => {
     const { value } = e.target;
@@ -15,12 +16,10 @@ function App() {
 
   useEffect(() => {
     if (inputVal === '') return;
-    const timer = setTimeout(() => {
-      fetchData()
-    }, 500)
-
-    return () => {
-      clearTimeout(timer)
+    const now = new Date()
+    if (now - inpref.current >= 500) {
+      fetchData();
+      inpref.current=now
     }
   }, [inputVal])
 
