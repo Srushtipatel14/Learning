4a1deec34eaa5966812dea6a7b16780404cad909
Srushtipatel14 <srushtip579@gmail.com>
2026-04-03 17:47:20 +0530

    change in UI

--- frontend/src/component/countData.js
+++ frontend/src/component/countData.js
@@ -2,7 +2,7 @@ import { appSelector } from "../customHooks/hooks";
 const DispalyCount = () => {
     const count = appSelector((state) => state.counter.value);
     return (
-        <h2>Count : {count}</h2>
+        <div style={{fontSize:"24px",fontWeight:"bold",marginTop:"20px"}}>Count : {count}</div>
     )
 }
 
