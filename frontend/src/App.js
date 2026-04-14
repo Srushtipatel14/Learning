6738c74b2bfd69b739ca97f8782429c4773e3800
Srushtipatel14 <srushtip579@gmail.com>
2026-04-14 23:00:49 +0530

    stop watch component done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,8 @@
-import React, { useCallback, useMemo, useState } from 'react'
-
-const Child = React.memo((data) => {
-  console.log("child render",data.data.name)
-  return (
-    <h4>child</h4>
-  )
-})
-
+import Countdown from "./Components/countdown"
+import "./App.css"
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount((prev) => prev + 1)
-  }
-
-  const dataVal=useMemo(()=>{
-    return {
-      name:"situ"
-    }
-  },[])
-
   return (
-    <div>
-      <Child data={dataVal} />
-      <h1>Count : {count}</h1>
-      <button onClick={handleClick}>Increse</button>
-    </div>
+    <Countdown/>
   )
 }
 
