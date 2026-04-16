af7c62ad6c2f5c2d48f02ab5ea4a3a29670ebf38
Srushtipatel14 <srushtip579@gmail.com>
2026-04-16 11:52:56 +0530

    example of infinite scrolling

--- frontend/src/Components/display.js
+++ frontend/src/Components/display.js
@@ -1,41 +1,75 @@
 import { useEffect, useRef, useState } from "react";
 
 const Display = () => {
+    const [items, setItems] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+    const timeRef = useRef(null);
 
-  const [status, setStatus] = useState(false);
-  const timeRef = useRef(null);
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            const resData = await fetch(
+                `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`,
+            );
+            const data = await resData.json();
 
+            if (data.length === 0) {
+                setHasMore(false);
+                return;
+            }
+            setItems((prev) => [...prev, ...data]);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-  useEffect(() => {
-    timeRef.current = new Worker(new URL(('./worker.js'), import.meta.url));
-    timeRef.current.onmessage = (e) => {
-      alert(e.data);
-    }
+    useEffect(() => {
+        if (hasMore) {
+            fetchData();
+        }
+    }, [page]);
 
-    return () => {
-      if(timeRef.current){
-        timeRef.current.terminate();
-      }
-    }
-  }, [])
+    useEffect(() => {
+        if (loading && !hasMore) return;
+        const observer = new IntersectionObserver(
+            (entry) => {
+                if (entry[0].isIntersecting && hasMore) {
+                    setPage((prev) => prev + 1);
+                }
+            },
+            { threshold: 1 },
+        );
+        const time = timeRef.current;
+        if (timeRef.current) {
+            observer.observe(time);
+        }
+        return () => {
+            observer.unobserve(time);
+        };
+    }, [loading, hasMore]);
 
-  const handleCalculation = () => {
-    if (timeRef.current) {
-      timeRef.current.postMessage("send")
-    }
-  }
+    return (
+        <div className="conatiner">
+            <div className="cards">
+                {items.map((item, index) => (
+                    <div className="card" key={index}>{item?.title}</div>
+                ))}
+            </div>
 
-  const chnageDisplay = () => {
-    setStatus((prev) => !prev)
-  }
-  return (
-    <div className="container" style={{ backgroundColor: status ? 'red' : "white" }}>
-      <div className="sub2">
-        <button onClick={handleCalculation}>Calculation</button>
-        <button onClick={chnageDisplay}> Display</button>
-      </div>
-    </div>
-  )
-}
+            <div ref={timeRef}>
+                {loading && (
+                    <div className="loading"></div>
+                )}
+            </div>
+            {!hasMore && (
+                <div className="nomore">No More Data Available</div>
+            )}
+        </div>
+    );
+};
 
 export default Display;
